579afc6bbe14e63343fd41ee404e26a5016276ad
Cesar_Paulino <cesar_paulino@icloud.com>
2025-01-30 13:43:22 -0600

    Create habilitarAdmin.bat
    comando net user

---
+++ habilitarAdmin.bat
@@ -0,0 +1,26 @@
+@echo off
+Title Comando para Habilitar Administrador
+@echo off
+@echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo ::::::::                                                ::::::::
+@echo :::::::: Habilitar cuenta de Administradopr             ::::::::
+@echo ::::::::                                                ::::::::
+@echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo ::                                                            ::
+@echo ::                   Apasionado de las TICs                   ::
+@echo ::         comando basico para habilitarla la cuenta de       ::
+@echo ::           administrador y establecer contraseña            ::
+@echo ::             $author Ing._Cesar_Paulino.dll                 ::
+@echo ::                                                            ::
+@echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+@pause
+
+echo Habilitando la cuenta de administrador...
+net user Administrator /active:yes
+
+echo Estableciendo contraseña para la cuenta de administrador...
+net user Administrator "Eco.1021*"
+
+echo ¡Listo! La cuenta de administrador ha sido habilitada y se ha establecido una contraseña.
+pause
